8e706f9c34921281a8acf80060f986b1729c700b
Akshat Ahuja <akshatahuja03@gmail.com>
2018-08-14 05:49:23 +0530

    latest commmit on the app

--- app/src/main/java/apackage/magicalstationary/com/magicalstationary/MainActivity.java
+++ app/src/main/java/apackage/magicalstationary/com/magicalstationary/MainActivity.java
@@ -3,7 +3,11 @@ package apackage.magicalstationary.com.magicalstationary;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +18,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View view) {
-        Intent i = new Intent(getApplicationContext(),Login.class);
+        Intent i = new Intent(getApplicationContext(), Login.class);
         startActivity(i);
     }
 
     public void onClickRegister(View view) {
-        Intent i = new Intent(getApplicationContext(),NewUser.class);
+        Intent i = new Intent(getApplicationContext(), NewUser.class);
         startActivity(i);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exit:
+                Toast.makeText(MainActivity.this, "exit is Selected", Toast.LENGTH_SHORT).show();
+                System.exit(0);
+                return true;
+
+            case R.id.newUser:
+                Toast.makeText(MainActivity.this, "New User registering", Toast.LENGTH_SHORT).show();
+                onClickRegister(findViewById(R.id.newUser));
+                return true;
+
+            case R.id.login:
+                Toast.makeText(MainActivity.this, "Loging In", Toast.LENGTH_SHORT).show();
+                onClickLogin(findViewById(R.id.login));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/apackage/magicalstationary/com/magicalstationary/Products.java
+++ app/src/main/java/apackage/magicalstationary/com/magicalstationary/Products.java
@@ -1,9 +1,12 @@
 package apackage.magicalstationary.com.magicalstationary;
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -27,60 +30,109 @@ public class Products extends AppCompatActivity {
     RadioButton rb;
     RadioGroup rg1;
     Button buys;
+    NotificationManagerCompat notificationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
         rg1 = (RadioGroup) findViewById(R.id.rg);
         buys = (Button) findViewById(R.id.buy);
+        final Product1 pen = new Product1();
+        final Product2 pencil = new Product2();
+        final Product3 notebook = new Product3();
+        final Product4 eraser = new Product4();
+        notificationManager = NotificationManagerCompat.from(this);
+        sendOnChannel1();
 
-
-    }
-
-
+    buys.setOnClickListener(new View.OnClickListener() {
         @Override
-        public boolean onCreateOptionsMenu(Menu menu)
-        {
-            MenuInflater menuInflater = getMenuInflater();
-            menuInflater.inflate(R.menu.menu, menu);
-            return true;
-        }
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item)
-        {
-            switch (item.getItemId())
-            {
-                case R.id.pen:
-
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.fragment, pen);
-                    fragmentTransaction.commit();
-
-                    Toast.makeText(Products.this, "Bookmark is Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.pencil:
-
-                    FragmentTransaction Transaction = getSupportFragmentManager().beginTransaction();
-                    Transaction.replace(R.id.fragment, pencil);
-                    Transaction.commit();
-
-                    Toast.makeText(Products.this, "Save is Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-
-                case R.id.notebook:
-                    FragmentTransaction fragment = getSupportFragmentManager().beginTransaction();
-                    fragment.replace(R.id.fragment,notebook);
-                    fragment.commit();
-
-                    Toast.makeText(Products.this, "Search is Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-
-                    case R.id.eraser:
-                    Toast.makeText(Products.this, "Share is Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                default:
-                    return super.onOptionsItemSelected(item);
+        public void onClick(View view) {
+//            sendOnChannel1();
+            int selectedId = rg1.getCheckedRadioButtonId();
+            rb = (RadioButton)findViewById(selectedId);
+            if(rb.getText().toString().equals("Pen")){
+                sendOnChannel1();
+                Toast.makeText(Products.this,rb.getText(),Toast.LENGTH_SHORT).show();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,pen);
+                fragmentTransaction.commit();
+            }
+            else if(rb.getText().toString().equals("Pencil")){
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,pencil);
+                fragmentTransaction.commit();
+            }
+            else if(rb.getText().toString().equals("NoteBook")){
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,notebook);
+                fragmentTransaction.commit();
+            }
+            else if(rb.getText().toString().equals("Eraser")){
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment,eraser);
+                fragmentTransaction.commit();
             }
         }
-
+    });
     }
+    public void sendOnChannel1() {
+        Notification notification = new NotificationCompat.Builder(Products.this, app.CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Notification Title")
+                .setContentText("This is my notification text")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE).build();
+
+        notificationManager.notify(1,notification);
+    }
+
+}
+
+
+
+//        @Override
+//        public boolean onCreateOptionsMenu(Menu menu)
+//        {
+//            MenuInflater menuInflater = getMenuInflater();
+//            menuInflater.inflate(R.menu.menu, menu);
+//            return true;
+//        }
+//        @Override
+//        public boolean onOptionsItemSelected(MenuItem item)
+//        {
+//            switch (item.getItemId())
+//            {
+//                case R.id.pen:
+//
+//                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//                    fragmentTransaction.replace(R.id.fragment, pen);
+//                    fragmentTransaction.commit();
+//
+//                    Toast.makeText(Products.this, "Bookmark is Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.pencil:
+//
+//                    FragmentTransaction Transaction = getSupportFragmentManager().beginTransaction();
+//                    Transaction.replace(R.id.fragment, pencil);
+//                    Transaction.commit();
+//
+//                    Toast.makeText(Products.this, "Save is Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//
+//                case R.id.notebook:
+//                    FragmentTransaction fragment = getSupportFragmentManager().beginTransaction();
+//                    fragment.replace(R.id.fragment,notebook);
+//                    fragment.commit();
+//
+//                    Toast.makeText(Products.this, "Search is Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//
+//                    case R.id.eraser:
+//                    Toast.makeText(Products.this, "Share is Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                default:
+//                    return super.onOptionsItemSelected(item);
+//            }
+
+
+
